51fa4cb8c8f6190dc2a2c4d42fff5d4133c719f6
jkals4689 <4689asgf@naver.com>
2023-04-26 18:10:51 +0900

    첫 시작 화면 제작

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+class RandomInt {
+    public int generateRandomNumber() {
+        Random rand = new Random();
+        int number = rand.nextInt(3);
+        return number;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        new Main_Window("Mini Game");
+
+    }
+
+}
---
+++ src/Main_Window.java
@@ -0,0 +1,49 @@
+import javax.swing.*;
+
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Main_Window extends JFrame {
+    JPanel pan = new JPanel();
+    JPanel contentPanel = new JPanel();
+    JLabel sel_Game = new JLabel("Select Game");
+
+    Main_Window(String str) {
+        super(str);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        JButton rpS = new JButton("가위 바위 보");
+        JButton mJB = new JButton("묵 찌 빠");
+
+        rpS.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                setVisible(false);
+            }
+
+        });
+
+        Box buttonBox = Box.createHorizontalBox();
+        Box labelBox = Box.createHorizontalBox();
+
+        sel_Game.setFont(sel_Game.getFont().deriveFont(30.0f));
+        labelBox.add(sel_Game);
+
+        buttonBox.add(rpS).setMaximumSize(new Dimension(150, 150));
+        buttonBox.add(Box.createHorizontalStrut(50));
+        buttonBox.add(mJB).setMaximumSize(new Dimension(150, 150));
+
+        pan.setLayout(new BoxLayout(pan, BoxLayout.Y_AXIS));
+        pan.add(Box.createVerticalStrut(50));
+        pan.add(labelBox);
+        pan.add(buttonBox);
+
+        add(pan);
+
+        setResizable(false);
+        setSize(500, 500);
+        setVisible(true);
+    }
+}
